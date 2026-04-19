a9fedd3e3027611c85175376c2e1fe0821249bf9
JayyyXp <49588855+JayyyXp@users.noreply.github.com>
2026-04-19 22:14:58 +0800

    Update RMS normalization (PyTorch 4/4)

--- model/rms_normalization.py
+++ model/rms_normalization.py
@@ -7,7 +7,9 @@ class Solution:
         # Implement RMS Normalization (similar to LayerNorm but without mean centering or beta)
         # Normalize x, then scale by gamma
         # Return result rounded to 4 decimal places as a list
-        N = len(x)
-        RMS = ((1 / N) * sum([xi**2 for xi in x]) + eps) ** 0.5
-        x_hat = [xi / RMS for xi in x]
-        return [round(x_hat_i * gamma_i, 4) for x_hat_i, gamma_i in zip(x_hat, gamma)]
+        x = np.array(x)
+        gamma = np.array(gamma)
+        RMS = np.sqrt(np.mean(np.pow(x, 2)) + eps)
+        x_hat = x / RMS
+
+        return np.round(gamma * x_hat, 4)
